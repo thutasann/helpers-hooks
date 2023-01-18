ef06c56be0a657b5150ae619b79d4ccad032b61a
thutasann <66902308+thutasann@users.noreply.github.com>
2023-01-18 17:01:02 +0630

    Refactor: refactoring mdx files

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Helpers Hooks</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/thutasann/react-helpers-hooks',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/thutasann/react-helpers-hooks',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Helpers Hooks',
   },
 }
 
