79155b1226012aae29d535204a1f34460f463fd5
thutasann <66902308+thutasann@users.noreply.github.com>
2023-01-20 00:34:09 +0630

    Func: useAccordion

--- components/test.tsx
+++ components/test.tsx
@@ -2,3 +2,15 @@ export const Welcome = () =>{
     console.log("This file is For the TSX formatting")
 }
 
+// ------------- PlayGround ---------------
+import { MutableRefObject } from 'react';
+
+type Accordion ={
+    ref: MutableRefObject<any>
+    expanded :boolean
+    toggle: () => void 
+    setCollapseState?: () => void
+}
+
+
+
