cb67195a71c63a35b2f95c73a9fbcb0c855338f5
thutasann <thutasann2002@gmail.com>
2023-01-20 19:27:38 -0800

    Func : Array Recursive

--- components/test.tsx
+++ components/test.tsx
@@ -1,29 +1,4 @@
 // This file is For the TSX formatting
-// @ts-ignore
-const Welcome = () =>{
+export const Welcome = () =>{
     console.log("Welcome From my Doc")
-}
-
-export type subMenus ={
-    menuId: string;
-    routeUrl: string;
-    subMenus: subMenus[];
-}
-
-export type MenuProps = {
-    menuId: string;
-    routeUrl: string;
-    subMenus: subMenus[];
-}[];
-
-export function FilterNestedArray(menus: MenuProps){
-    let flattendedMenus: subMenus[] = [];
-
-    function flatMenusRecursive(m: any){
-        for (let i of m){
-            flattendedMenus.push(i);
-            if(i.subMenus?.length){
-            }
-        }
-    }
 }
