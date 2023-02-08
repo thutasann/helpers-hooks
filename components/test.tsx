3207a75e100ac38a51ead039021b28095abc2d20
thutasann <thutasann2002@gmail.com>
2023-02-08 17:45:46 +0630

    Helper: Canvas

--- components/test.tsx
+++ components/test.tsx
@@ -1,4 +1,14 @@
+import { useRef, useState } from 'react'
+
 // This file is For the TSX formatting
 export const Welcome = () => {
   console.log('Welcome From my Doc')
+}
+
+export function isCanvasEmpty(canvas: HTMLCanvasElement | null) {
+  if (!canvas) return true
+  const blankCanvas = document.createElement('canvas')
+  blankCanvas.width = canvas.width
+  blankCanvas.height = canvas.height
+  return canvas.toDataURL() === blankCanvas.toDataURL()
 }
